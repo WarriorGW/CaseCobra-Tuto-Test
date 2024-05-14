188912ead4c50f4173fe97dd3b6995e31fea8091
WarriorGW <ferguerrero.ordonez@gmail.com>
2024-05-13 18:27:30 -0600

    Maybe last commit

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -4,6 +4,10 @@ import { create } from "domain"
 import { headers } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
 import Stripe from "stripe"
+import { Resend } from "resend"
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail"
+
+const resend = new Resend(process.env.RESEND_API_KEY!)
 
 export async function POST(req: NextRequest) {
   try {
@@ -37,7 +41,7 @@ export async function POST(req: NextRequest) {
       const billingAddress = session.customer_details?.address
       const shippingAddress = session.shipping_details?.address
 
-      await db.order.update({
+      const updatedOrder = await db.order.update({
         where: {
           id: orderId,
         },
@@ -64,6 +68,25 @@ export async function POST(req: NextRequest) {
             },
           },
         },
+      })
+
+      await resend.emails.send({
+        from: "CaseCobra <ferweb.rordo@gmail.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order!",
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state!,
+          },
+        }),
       })
     }
 
---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import React, { Suspense } from "react"
+import ThankYou from "./ThankYou"
+
+function ThankYouPage() {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  )
+}
+
+export default ThankYouPage
